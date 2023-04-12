7ece6b21eda15c48f65fac45163e880fc4a27119
Christian Emard Aniag <aniagchristianemard@gmail.com>
2023-04-12 15:55:15 +0800

    Add Hall Sensor

--- azeoscandado.ino
+++ azeoscandado.ino
@@ -15,20 +15,27 @@
 #define SENSOR_IN D1
 #define ALARM_PIN D2
 #define SERVO_PIN D3
+#define HALL_PIN D5
 
 #define LOCK_POWER_BL V0
 #define SENSITIVITY_BL V1
 #define SERVO_BL V2
+#define UNLOCK_DELAY_BL V3
+
+#define LOCK 0
+#define UNLOCK 180
 
 Servo myservo;
 const char auth[] = BLYNK_AUTH_TOKEN;
 unsigned long previousMillisDelay = 0;
 const long intervalDelay = 500;
-unsigned long alarmPreviousMillisDelay = 0;
-long alarmIntervalDelay = 50;
+unsigned long unlockedMillisDelay = 0;
+long unlockedIntervalDelay = 5000;
 uint sensitivity = 3;
 uint sensor_power = 1;
 uint triggerCtr = 0;
+uint unlockedDelay = 3;
+bool isLocked = false, flag = true;
 
 BLYNK_CONNECTED() {
   Blynk.syncAll();
@@ -44,10 +51,14 @@ BLYNK_WRITE(SENSITIVITY_BL) {
   Serial.println("SENSITIVITY_BL: " + String(sensitivity));
 }
 
+BLYNK_WRITE(UNLOCK_DELAY_BL) {
+  unlockedDelay = param.asInt();
+  Serial.println("UNLOCK_DELAY_BL: " + String(unlockedDelay));
+}
+
 BLYNK_WRITE(SERVO_BL) {
-  int servo = param.asInt();
-  Serial.println("SERVO: " + String(servo));
-  servo ? myservo.write(180) : myservo.write(0);  
+  isLocked = param.asInt();
+  Serial.println("SERVO: " + String(isLocked));
 }
 
 void setup() {
@@ -101,6 +112,7 @@ void setup() {
 
   myservo.attach(SERVO_PIN);
   pinMode(SENSOR_IN, INPUT);
+  pinMode(HALL_PIN, INPUT);
   pinMode(ALARM_PIN, OUTPUT);
   Blynk.virtualWrite(SENSITIVITY_BL, sensitivity);
   Blynk.virtualWrite(LOCK_POWER_BL, HIGH);
@@ -117,10 +129,10 @@ void loop() {
       int read = digitalRead(SENSOR_IN);
       if (read == HIGH) {
         if (triggerCtr < 10) triggerCtr++;
-        Serial.println("Vibrating...");
+        Serial.print("Vibrating: ");
       } else {
         if (triggerCtr > 0) triggerCtr--;
-        Serial.println("No vibration");
+        Serial.print("No vibration: ");
       }
       Serial.println(triggerCtr);
       if (triggerCtr >= sensitivity) {
@@ -137,4 +149,41 @@ void loop() {
     digitalWrite(ALARM_PIN, LOW);
     triggerCtr = 0;
   }
+
+  hallSensor();
+}
+
+void hallSensor() {
+  bool sensorStatus = digitalRead(HALL_PIN);
+  if (sensorStatus && flag) {
+    lockServo();
+    flag = false;
+  } else if (!isLocked && !flag) {
+    unlockServo();
+    if (!sensorStatus) {
+      flag = true;
+    }
+    delay(unlockedDelay * 1000);
+    sensorStatus = digitalRead(HALL_PIN);
+    if (sensorStatus) {
+      flag = true;
+    }
+  } else if (isLocked && !sensorStatus) {
+    Serial.println("Warning");
+    Blynk.logEvent("alarm", "Your padlock was not successfully locked. Please check the lock and try again to ensure the security of your item.");
+  }
+}
+
+void unlockServo() {
+  myservo.write(UNLOCK);
+  Blynk.virtualWrite(SERVO_BL, LOW);
+  isLocked = false;
+  Serial.println("unlocked");
+}
+
+void lockServo() {
+  myservo.write(LOCK);
+  Blynk.virtualWrite(SERVO_BL, HIGH);
+  isLocked = true;
+  Serial.println("locked");
 }
